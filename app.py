3fa3d11ae73aa4373e29e661fcf2ebebc93429b5
阿牛 <484978101@qq.com>
2023-04-23 15:10:17 +0800

    修bug

--- app.py
+++ app.py
@@ -72,4 +72,4 @@ def chat():
 
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000)
--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@
 SECRET_KEY = "asghnjadfbtywyn"
 
 # openAi api key
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = ""
 
 # openAi 官方 api
 # URL = "https://api.openai.com/v1/chat/completions"
