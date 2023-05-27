9321b5d3498fac9a0527318a2f48d0ebb709f638
Aniuyyds <90816904+Aniuyyds@users.noreply.github.com>
2023-05-27 20:47:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 from flask import Flask, request, jsonify, render_template, Response
 import requests
 import json
+import os
 
 app = Flask(__name__)
 
@@ -21,7 +22,7 @@ def chat():
         return jsonify({"error": {"message": "请输入prompts！", "type": "invalid_request_error", "code": ""}})
 
     if apiKey is None:
-        apiKey = app.config['OPENAI_API_KEY']
+        apiKey = os.environ.get('OPENAI_API_KEY')
 
     headers = {
         "Content-Type": "application/json",
