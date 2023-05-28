23fe362106e687d6acb9186b836a8921dad17e5f
Aniuyyds <90816904+Aniuyyds@users.noreply.github.com>
2023-05-28 14:55:29 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ SECRET_KEY = "asghnjadfbtywyn"
 OPENAI_API_KEY = ""
 
 # openAi 官方 api
-# URL = "https://api.openai.com/v1/chat/completions"
+URL = "https://api.openai.com/v1/chat/completions"
 
 # openAi 代理 api
-URL = "https://open.aiproxy.xyz/v1/chat/completions"
+# URL = "https://open.aiproxy.xyz/v1/chat/completions"
