fe42e40e12566e0b7e7cb39bb3430d6d4d582d17
Aniuyyds <90816904+Aniuyyds@users.noreply.github.com>
2025-02-06 15:16:45 +0800

    Update app.py
    适配deepseek R1推理过程

--- app.py
+++ app.py
@@ -9,20 +9,27 @@ app = Flask(__name__)
 # 从配置文件中settings加载配置
 app.config.from_pyfile('settings.py')
 
+
 @app.route("/", methods=["GET"])
 def index():
     return render_template("chat.html")
+
 
 @app.route("/chat", methods=["POST"])
 def chat():
     messages = request.form.get("prompts", None)
     apiKey = request.form.get("apiKey", None)
+    apiUrl = request.form.get("apiUrl", None)
     model = request.form.get("model", "gpt-3.5-turbo")
     if messages is None:
         return jsonify({"error": {"message": "请输入prompts！", "type": "invalid_request_error", "code": ""}})
 
+    # 依次从环境变量、配置文件获取key和代理url
     if apiKey is None:
-        apiKey = os.environ.get('OPENAI_API_KEY',app.config["OPENAI_API_KEY"])
+        apiKey = os.environ.get('OPENAI_API_KEY', app.config["OPENAI_API_KEY"])
+
+    if apiUrl is None:
+        apiUrl = os.environ.get("OPENAI_API_URL", app.config["URL"])
 
     headers = {
         "Content-Type": "application/json",
@@ -44,7 +51,7 @@ def chat():
 
     try:
         resp = requests.post(
-            url=app.config["URL"],
+            url=apiUrl + "/v1/chat/completions",
             headers=headers,
             json=data,
             stream=True,
@@ -64,13 +71,24 @@ def chat():
                 except:
                     errorStr += streamStr.strip()  # 错误流式数据累加
                     continue
-                delData = streamDict["choices"][0]
-                if delData["finish_reason"] != None :
+
+                # 应对部分中转接口错误信息一次返回
+                try:
+                    delData = streamDict["choices"][0]
+                except:
+                    yield streamStr
+
+                if delData["finish_reason"] is not None:
                     break
                 else:
-                    if "content" in delData["delta"]:
+                    # 返回 deepseek R1 推理过程
+                    if "reasoning_content" in delData["delta"] and delData["delta"]["reasoning_content"] is not None:
+                        respStr = delData["delta"]["reasoning_content"]
+                    # 返回正式结果
+                    elif "content" in delData["delta"] and delData["delta"]["content"] is not None:
                         respStr = delData["delta"]["content"]
                         # print(respStr)
+                    if respStr is not None:
                         yield respStr
 
         # 如果出现错误，此时错误信息迭代器已处理完，app_context已经出栈，要返回错误信息，需要将app_context手动入栈
@@ -78,7 +96,8 @@ def chat():
             with app.app_context():
                 yield errorStr
 
-    return Response(generate(), content_type='application/octet-stream')
+    return Response(generate(), content_type='text/event-stream')
+
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    app.run(port=5000, debug=True)
